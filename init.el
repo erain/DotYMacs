65767a45d215951e597c8224e689cc7a7a1f24bc
Yu YI <yiyu@ymail.com>
2012-08-01 06:11:17 +0800

    adding the prolog section

--- init.el
+++ init.el
@@ -484,6 +484,14 @@
 ;; (setq inferior-lisp-program "sbcl")
 ;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
 
+(autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
+(autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
+(autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
+(setq auto-mode-alist (append '(("\\.pl$" . prolog-mode)
+                                ("\\.m$" . mercury-mode))
+;; (setq prolog-system 'swi)  ; optional, the system you are using;
+;;                            ; see `prolog-system' below for possible values
+                               auto-mode-alist))
 
 
 
