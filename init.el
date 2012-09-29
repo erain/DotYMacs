317257a4d4606ea6224fdd019d466a7df19575e0
Yu YI <yiyu@ymail.com>
2012-09-29 10:58:07 +0800

    OK, now I know adding sources is a one-time action...

--- init.el
+++ init.el
@@ -504,9 +504,9 @@
 
 
 ;; Add the original Emacs Lisp Package Archive
-(add-to-list 'package-archives
-             '("elpa" . "http://tromey.com/elpa/"))
-;; Add the user-contributed repository
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives
+;;              '("elpa" . "http://tromey.com/elpa/"))
+;; ;; Add the user-contributed repository
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
 
